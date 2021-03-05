adb0fa810493261642691cfda63f1c527412e277
isakK96 <isak.koski96@gmail.com>
2021-03-06 00:36:48 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1 className="">TASKIE</h1>
+      <div id="newTask" className="">
+
+      </div>
+      <div id="taskList" className="">
+
+      </div>
+      <div id="bottomMenu" className="">
+        <p>items left</p>
+        <div id="taskFilters" className="">
+
+        </div>
+        <p>Clear completed</p>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      light: {
+        BrightBlue: "hsl(220, 98%, 61%)",
+        VeryLightGray: "hsl(0, 0%, 98%)",
+        VeryLightGrayishBlue: "hsl(236, 33%, 92%)",
+        LightGrayishBlue: "hsl(233, 11%, 84%)",
+        DarkGrayishBlue: "hsl(236, 9%, 61%)",
+        VeryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+      },
+      dark: {
+        VeryDarkBlue: "hsl(235, 21%, 11%)",
+        VeryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+        LightGrayishBlue: "hsl(234, 39%, 85%)",
+        "LightGrayishBlue (hover)": "hsl(236, 33%, 92%)",
+        DarkGrayishBlue: "hsl(234, 11%, 52%)",
+        VeryDarkGrayishBlue: "hsl(233, 14%, 35%)",
+        VeryDarkGrayishBlue: "hsl(237, 14%, 26%)",
+      },
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
